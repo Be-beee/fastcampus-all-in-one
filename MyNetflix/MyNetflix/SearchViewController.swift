e3f02d422ba95274a9eb7aa5344c77c7289b2539
bokyungseo <maybutter756@gmail.com>
2020-11-04 16:57:36 +0900

    Update MyNetflix

---
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -0,0 +1,180 @@
+//
+//  SearchViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/02.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+import AVFoundation
+
+class SearchViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    var movies: [Movie] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+// MARK: - Collection View
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // movie
+        // player vc
+        // player vc + movie
+        
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.previewURL)!
+        let item = AVPlayerItem(url: url)
+        
+        let storyboard = UIStoryboard(name: "Player", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "PlayerViewController") as! PlayerViewController
+        vc.modalPresentationStyle = .fullScreen
+        
+        vc.player.replaceCurrentItem(with: item)
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = resultCollectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return UICollectionViewCell() }
+        
+        let movie = movies[indexPath.row]
+        let url = URL(string: movie.thumbnailPath)!
+        
+        cell.movieThumbnail.kf.setImage(with: url)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        
+        let width: CGFloat = (collectionView.bounds.width - margin*2 - itemSpacing*2)/3
+        let height: CGFloat = width * 10/7
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var movieThumbnail: UIImageView!
+}
+
+
+// MARK: - Searchbar
+extension SearchViewController: UISearchBarDelegate {
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder() // 키보드에서 관심 땐다..?
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        dismissKeyboard()
+        
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        // 네트워킹을 통한 검색
+        // 목표: 서치 텀을 가지고 네트워킹을 통해 영화 검색
+        // 검색 API 필요
+        // 결과를 받아올 모델 Movie, Response
+        // 결과 받아와서 CollectionView로 표현해주자
+        
+        SearchAPI.search(searchTerm) { movies in
+            // CollectionView로 표현하기
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
+            
+        }
+    }
+}
+
+class SearchAPI {
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
+        
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+
+        urlComponents.queryItems?.append(mediaQuery)
+        urlComponents.queryItems?.append(entityQuery)
+        urlComponents.queryItems?.append(termQuery)
+        
+        let dataTask = session.dataTask(with: urlComponents.url!) { data, response, error in
+            let successRange = 200 ..< 300
+            guard error == nil, let statusCode = (response as? HTTPURLResponse)?.statusCode, successRange.contains(statusCode) else {
+                completion([])
+                return
+            } // 에러 없고, 네트워킹에 성공했는지
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            } // 데이터 가져오기
+            
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
+            print("result: \(movies.count)")
+            
+        }
+        dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+    
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
+}
